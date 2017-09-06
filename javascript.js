376b1b434ca401afde6f0c67967d49188458fd4e
Sunit Kulkarni <sunitkulkarni@Sunits-MacBook-Pro-2.local>
2017-09-05 21:17:28 -0400

    adding more to hello function for undefined string

--- javascript.js
+++ javascript.js
@@ -1,10 +1,22 @@
-function positiveNum(numArray) {
-    length = numArray.length();
-    index = 0
-    if (index =< length){
-        
+function hello(name) {
+    if (name == undefined) {
+        console.log("Hello World!");
     } else {
-        
+        name.toString();
+        var output = "hello" + ", " + name;
+        return output;
     }
     
 }
+
+
+//function positiveNum(numArray) {
+//    length = numArray.length();
+//    index = 0
+//    if (index =< length){
+//        
+//    } else {
+//        
+//    }
+//    
+//}
