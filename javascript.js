9fc9fc0cf72e326805401431c2321e07b6fbcdc3
Sunit Kulkarni <sunitkulkarni@Sunits-MacBook-Pro-2.local>
2017-09-07 19:59:35 -0400

    added more functions

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 function hello(name) {
+    //try out the prompt function for user raw input
     if (name == undefined) {
         console.log("Hello World!");
     } else {
@@ -6,9 +7,48 @@ function hello(name) {
         var output = "hello" + ", " + name;
         return output;
     }
-    
 }
 
+function madlib(name, subject) {
+    if (name == undefined) {
+        console.log("Type in a name and subject please");
+    } else {
+        //name.toString();
+        var output = name + "'s" + " favorite subject in school is " + subject;
+        return output;
+    }
+}
+
+function tipAmount(dollarAmount, service) {
+    if (service == 'good'){
+        total = 0.20 * dollarAmount;
+        return total;
+    } else if (service == 'fair') {
+        total = 0.15 * dollarAmount;
+        return total;
+    } else if (service == 'bad') {
+        total = 0.10 * dollarAmount;
+        return total;
+    } else {
+        console.log("type in valid parameters please!")
+    }
+}
+
+function totalAmount(dollarAmount, service) {
+    return dollarAmount + tipAmount(dollarAmount, service);
+}
+
+function splitAmount(dollarAmount, service, people) {
+    return (dollarAmount + tipAmount(dollarAmount, service))/people;
+}
+
+function printNumbers(start, finish) {
+    for (i = start; i <= finish; i++) {
+        console.log(i);
+    }
+}
+
+function printSquare(length)
 
 //function positiveNum(numArray) {
 //    length = numArray.length();
